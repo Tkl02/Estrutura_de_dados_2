62dcb646a2e4057b4dff89c6491325ddcc2f7e49
Leonardo Faustino <84601712+Tkl02@users.noreply.github.com>
2022-09-21 13:28:26 -0300

    .

--- test.py
+++
@@ -1,9 +0,0 @@
-
-from dataclasses import replace
-
-
-insfos =['nome:','leonardo dias', 'algoritimo:', 'slect/insert sort']
-
-arr = insfos
-with open ("insert ordenado.txt", "w") as arquivo:
-    arquivo.write("{}".format(arr+insfos))
