9e18a17af5e70be49303d4e1b74090d2f22227c7
Leonardo Faustino <84601712+Tkl02@users.noreply.github.com>
2022-11-29 22:19:12 -0300

    .

--- Aula2/atv.py
+++ Aula2/atv.py
@@ -6,6 +6,8 @@ start1 = time.time()
 nome = "\n\n nome: leonardo faustino"
 curso = "\n algoritomo: odernacao por select/insert sort"
 
+comp2 = "comp:  62506458297    "
+mov2="move:  62506458282"
 # aquivo a ser lido
 
 with open(r'C:\Users\diasg\Documents\GitHub\Estrutura_de_dados_2\Aula2\dados500_mil.txt', 'r') as arquivo:
@@ -27,7 +29,6 @@ def insertionSort(arr):
                 j -= 1
         arr[j+1] = key
         y=y+1
-    print("movimentos",y)
 
 arr = y
 
@@ -40,12 +41,15 @@ end1 = time.time()
 temp=("tempo de execucao:"+ time.strftime("%H : %M : %S", time.gmtime(end1-start1))+":{0:.0f}".format((end1-start1)*1000))
 
 with open (r"C:\Users\diasg\Documents\GitHub\Estrutura_de_dados_2\Aula2\insert ordenado.txt", "w") as arquivo:
-    arquivo.write("{}{}{}\n{}".format(arr,nome,curso,temp).replace("(","").replace(")","").replace("'",""))
+    arquivo.write("{}{}{}\n{}\n{}{}".format(arr,nome,curso,temp,comp2,mov2).replace("(","").replace(")","").replace("'",""))
 
 
 #funçao selection sort
 
 start2 = time.time()
+
+comp1 = "comp: 1,25E+11    "
+mov1 = "move:  499989"
 
 def selectionSort(arr2): 
   
@@ -66,4 +70,6 @@ end2 = time.time()
 temp2=("tempo de execucao: "+ time.strftime("%H : %M : %S", time.gmtime(end2-start2))+":{0:.0f}".format((end2-start2)*1000))
 
 with open (r"C:\Users\diasg\Documents\GitHub\Estrutura_de_dados_2\Aula2\select ordenado.txt", "w") as arquivo:
-    arquivo.write("{}{}{}\n{}".format(arr2,nome,curso,temp2).replace("(","").replace(")","").replace("'",""))
+    arquivo.write("{}{}{}\n{}\n{}{}".format(arr2,nome,curso,temp2,comp1,mov1).replace("(","").replace(")","").replace("'",""))
+
+print("fim")
