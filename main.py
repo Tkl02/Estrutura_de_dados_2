d795e11c0c3687df6ab9292e8df79011c5ef7998
Leonardo Faustino <84601712+Tkl02@users.noreply.github.com>
2022-12-21 22:45:54 -0300

    .

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from algoritimos import *
 
 def ler_arquivo():
     with open('C:\\Users\\diasg\\Documents\\GitHub\\Estrutura_de_dados_2\\dados\\dados5.txt','r') as file:
@@ -5,6 +6,9 @@ def ler_arquivo():
     arr = dados.replace("[","").replace("]","").split(",")
     return arr
 
-def salver_arquivo(arr):
+def salvar_arquivo(arr):
+    nome = "Leonardo Faustino"
+    nomeAlgoritimo = "xxxxxxx"
     with open('C:\\Users\\diasg\\Documents\\GitHub\\Estrutura_de_dados_2\\dados\\dados5.txt','w') as file:
-        file.write(str(arr))
+        file.write("{}\n{}{}\n{}{}"format(dados,nome,nomeAlgoritimo,numeroTrocas,numeroComp))
+        
